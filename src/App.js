40523340a6d47de2289cd96211eb8d2ef04a051c
Alexander Aymond <lynxkhaos@gmail.com>
2022-04-20 16:16:17 -0500

    Assignment Complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
 import "./App.css";
+import NasaPhoto from "./Components/NasaPhoto.js";
+
+
 
 function App() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=hXchbLxB5kqRG0lTXuv2hB4wdRPAt1p5i4k6BIuk')
+    .then(res => {
+      setData(res.data)
+    })
+    .catch(err => console.error(err))
+  }, [])
+ 
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      { data && <NasaPhoto photo={data} /> }
     </div>
   );
 }
