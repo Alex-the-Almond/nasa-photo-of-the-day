10a905d94e340decc5bd06ea9b23dbc2646bc39c
Alexander Aymond <lynxkhaos@gmail.com>
2022-04-21 14:30:57 -0500

    Assigment Completed, added a date picker to webpage

--- src/App.js
+++ src/App.js
@@ -3,22 +3,42 @@ import axios from 'axios';
 import "./App.css";
 import NasaPhoto from "./Components/NasaPhoto.js";
 
+/*
+-create input as type date picker ( input type: date picker)
+-create an on change handler
+-implement state for the picker, would be best to do this
+at App.js level
+-look through API information to interpret the date in the URL
+-inject the date into the URL USING DOLLAR SIGN AND CURLY BOIS
+*/
+
 
 
 function App() {
+  
+  let today = new Date().toISOString().slice(0, 10)
+
   const [data, setData] = useState();
+  const [date, setDate] = useState(today);
+
+  
+
+  const onChangeHandler = (evt) => {
+   setDate(evt.target.value);
+  }
+  
 
   useEffect(() => {
-    axios.get('https://api.nasa.gov/planetary/apod?api_key=hXchbLxB5kqRG0lTXuv2hB4wdRPAt1p5i4k6BIuk')
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=hXchbLxB5kqRG0lTXuv2hB4wdRPAt1p5i4k6BIuk&date=${date}`)
     .then(res => {
       setData(res.data)
     })
     .catch(err => console.error(err))
-  }, [])
+  }, [date])
  
   return (
     <div className="App">
-      { data && <NasaPhoto photo={data} /> }
+      { data && <NasaPhoto photo={data} onChangeHandler={onChangeHandler} date={date} today={today} /> }
     </div>
   );
 }
--- src/Components/NasaPhoto.js
+++ src/Components/NasaPhoto.js
@@ -1,14 +1,61 @@
 import React from 'react';
+import styled from 'styled-components'
 
+const StyledDiv= styled.div`
+    background-image: url('https://images.unsplash.com/photo-1534796636912-3b95b3ab5986?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1771&q=80');
+    background-size: auto 100%;
+    color: white;
+    background-size: cover;
+    height: 100vh;
+`
+
+const StyledImage = styled.img`
+   max-width: 60%;
+   display: flex;
+   align-items: center;
+
+   &:hover {
+       max-width: 80%;
+       border-radius: 10em;
+       border: solid 2px lightblue;
+   }
+ `
+
+const StyledH3 = styled.h3`
+    padding-top: 3em;
+    font-size: 2em;
+`   
+
+const StyledDate = styled.p`
+    font-size: 1.5em;
+`
+const StyledExp = styled.p`
+    padding-top: 1em;
+    font-size: 1em;
+    text-shadow: 2px 2px black;
+    
+
+    &:hover{
+        background-color: grey;
+        font-size: 1.4em;
+        }
+`
 
 const NasaPhoto = (props) => {
+    
+
+
+
+
     return (
-        <div className='nasa-photo-wrapper'>
-            <h3>{props.photo.title}</h3>
-            <p>{props.photo.date}</p>
-            <img src={props.photo.hdurl} alt='Space' />
-            <p>{props.photo.explanation}</p>
-        </div>
+        <StyledDiv className='nasa-photo-wrapper'>
+            <StyledH3>{props.photo.title}</StyledH3>
+            <label>Date Picker: </label>
+            <input type='date' onChange={props.onChangeHandler} max={props.today} value={props.date}/>
+            <StyledDate>{props.photo.date}</StyledDate>
+            <StyledImage src={props.photo.hdurl} alt='Space' />
+            <StyledExp>{props.photo.explanation}</StyledExp>
+        </StyledDiv>
     )
 };
 
